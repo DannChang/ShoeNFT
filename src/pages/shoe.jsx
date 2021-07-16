b57709ca162e75e61f1ad2f1101549c57d92eec7
Dan Chang <dmagma@hotmail.com>
2021-07-16 10:31:24 -0700

    New compressed shoe model + orbit control

--- src/pages/shoe.jsx
+++ src/pages/shoe.jsx
@@ -1,21 +1,25 @@
 import Instructions from '@/components/dom/instructions'
 import dynamic from 'next/dynamic'
-// import Shoe from '@/components/canvas/Shoe'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
-const Shoe = dynamic(() => import('@/components/canvas/ShoeOne'), {
+const Shoe = dynamic(() => import('@/components/canvas/Shoe'), {
   ssr: false,
 })
 
 const Page = () => {
   return (
     <>
-        <Canvas>
+        <Canvas
+            // camera={{ position: [0, 0, 0], zoom: 0, up: [0, 0, 0] }}
+        >
+            <OrbitControls />
             <ambientLight intensity={0.7}/>
             <Suspense fallback={null}>
                 <Shoe r3f/>
             </Suspense>
+
         </Canvas>
       <Instructions />
     </>
